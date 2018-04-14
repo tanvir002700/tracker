ba699f7d66e7159b5c0ee8ffd7ed576c00c941cb
tanvir hasan <tanvir002700@gmail.com>
2018-04-14 21:35:10 +0600

    change templates

--- office_tracker/accounts/views.py
+++ office_tracker/accounts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class AccountDetailView(TemplateView):
-  template_name = 'account_detail.html'
+  template_name = 'accounts/account_detail.html'
