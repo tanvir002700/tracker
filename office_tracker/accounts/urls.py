b8bbd99d7c8b2cdb6331bc87e92dcff657d73560
tanvir hasan <tanvir002700@gmail.com>
2018-04-14 22:12:44 +0600

    create user creation form

--- office_tracker/accounts/urls.py
+++ office_tracker/accounts/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^detail/$', views.AccountDetailView.as_view(), name='detail')
+  url(r'^detail/$', views.AccountDetailView.as_view(), name='detail'),
+  url(r'^registration/$', views.RegistrationView.as_view(), name='registration')
 ]
 
--- office_tracker/accounts/views.py
+++ office_tracker/accounts/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
+from .forms import RegistrationForm
+
+
+class RegistrationView(FormView):
+  template_name = 'accounts/registration.html'
+  form_class = RegistrationForm
+
+  def post(self, request, *args, **kwargs):
+    pass
+
 
 class AccountDetailView(TemplateView):
   template_name = 'accounts/account_detail.html'
