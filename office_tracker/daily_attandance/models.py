9d2d09e4937aa1ec35bbf11a4e80cfbb5d8dd109
tanvir hasan <tanvir002700@gmail.com>
2018-04-19 23:35:21 +0600

    add attandance model for daily attandance app

--- office_tracker/daily_attandance/models.py
+++ office_tracker/daily_attandance/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from core.models import TimeStampedModel
 
-# Create your models here.
+
+class Attandance(TimeStampedModel):
+    enter_at = models.DateTimeField(null=False)
+    out_at = models.DateTimeField(null=False)
+
