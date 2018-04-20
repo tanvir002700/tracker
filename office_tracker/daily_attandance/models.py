504b6f1784e8ab973fa5be6acc95376761fccda0
tanvir hasan <tanvir002700@gmail.com>
2018-04-20 14:45:54 +0600

    revert migration

--- office_tracker/daily_attandance/models.py
+++ office_tracker/daily_attandance/models.py
@@ -6,4 +6,3 @@ from accounts.models import User
 class Attandance(TimeStampedModel):
     enter_at = models.DateTimeField(null=False)
     out_at = models.DateTimeField(null=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
