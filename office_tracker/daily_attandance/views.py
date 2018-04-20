b6a99874f5eac96d4518c5b73e36121cce953161
tanvir hasan <tanvir002700@gmail.com>
2018-04-21 00:18:41 +0600

    add redirect view for daily login

--- office_tracker/daily_attandance/views.py
+++ office_tracker/daily_attandance/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic.base import RedirectView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class DailyLoginView(LoginRequiredMixin, RedirectView):
+    url = reverse_lazy('accounts:detail')
+
+    def get_redirect_url(self, *args, **kwargs):
+        print("come here")
+        return super(DailyLoginView, self).get_redirect_url(*args, **kwargs)
