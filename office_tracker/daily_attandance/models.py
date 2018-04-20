230ee94d7e6244d3af1279f6fffa60351cd2482b
tanvir hasan <tanvir002700@gmail.com>
2018-04-20 14:34:03 +0600

    add one to many relation in attandance

--- office_tracker/daily_attandance/models.py
+++ office_tracker/daily_attandance/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from core.models import TimeStampedModel
+from accounts.models import User
 
 
 class Attandance(TimeStampedModel):
     enter_at = models.DateTimeField(null=False)
     out_at = models.DateTimeField(null=True)
-
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
