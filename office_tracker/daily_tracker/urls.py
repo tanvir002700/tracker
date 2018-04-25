c0e833c5de33c023b3a6cd557f5c156031308621
tanvir hasan <tanvir002700@gmail.com>
2018-04-25 23:09:51 +0600

    install daily logout url

--- office_tracker/daily_tracker/urls.py
+++ office_tracker/daily_tracker/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^login/$', views.DailyLoginView.as_view(), name='login')
+    url('^login/$', views.DailyLoginView.as_view(), name='login'),
+    url('^logout/$', views.DailyLogoutView.as_view(), name='logout')
 ]
