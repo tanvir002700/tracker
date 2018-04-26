feb8c80f727d8c34d7ccd8f59a7d8fba06b2e4e1
tanvir hasan <tanvir002700@gmail.com>
2018-04-27 02:23:56 +0600

    write first test spce

--- office_tracker/accounts/tests.py
+++ office_tracker/accounts/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser
 
-# Create your tests here.
+from .models import User
+from .views import AccountDetailView
+
+
+class SimpleTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='jacob', email='jacob@test.com', password='top_secret')
+
+    def test_details(self):
+        request = self.factory.get('/accounts/detail')
+        request.user = self.user
+
+        response = AccountDetailView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
