c3a1851e26e44bf84fee7c4757261e931bc419b7
tanvir hasan <tanvir002700@gmail.com>
2018-04-26 20:53:40 +0600

    add timezone middleware

---
+++ office_tracker/core/middleware.py
@@ -0,0 +1,14 @@
+import pytz
+
+from django.utils import timezone
+from django.utils.deprecation import MiddlewareMixin
+
+class TimezoneMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        print("working.................................")
+        tzname = request.session.get('django_timezone')
+        if tzname:
+            timezone.activate(pytz.timezone(tzname))
+        else:
+            timezone.activate(pytz.timezone('Asia/Dhaka'))
+
--- office_tracker/office_tracker/settings.py
+++ office_tracker/office_tracker/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middleware.TimezoneMiddleware'
 ]
 
 ROOT_URLCONF = 'office_tracker.urls'
