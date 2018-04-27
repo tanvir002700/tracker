34d405633c0e0c7d7a9283e3bbb9caecb13b2368
tanvir hasan <tanvir002700@gmail.com>
2018-04-27 20:07:06 +0600

    add template debuger setup

--- office_tracker/office_tracker/settings.py
+++ office_tracker/office_tracker/settings.py
@@ -68,7 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ]
+            ],
+            'string_if_invalid': 'INVALID EXPRESSION: %s'
         },
     },
 ]
