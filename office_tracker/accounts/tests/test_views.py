e2cd41321b63c3ed51b63a2bf71284acc16fdc9c
tanvir hasan <tanvir002700@gmail.com>
2018-04-29 00:50:39 +0600

    add full spec for login view

--- office_tracker/accounts/tests/test_views.py
+++ office_tracker/accounts/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import AnonymousUser
 
@@ -9,13 +10,20 @@ class SimpleTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.user = User.objects.create_user(username='jacob', email='jacob@test.com', password='top_secret')
+        self.credentials = {
+            'username': 'jacob',
+            'password': 'top_secret'
+        }
 
-    def test_login(self):
-        request = self.factory.get('/accounts/login')
-        request.user = AnonymousUser
+    def test_login_get(self):
+        response = self.client.get(reverse('accounts:login'))
 
-        response = LoginView.as_view()(request)
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_post(self):
+        response = self.client.post(reverse('accounts:login'), **self.credentials, follow=True)
+
+        self.assertEqual(response.status_code, 200)
 
     def test_details(self):
         request = self.factory.get('/accounts/detail')
--- office_tracker/core/middleware.py
+++ office_tracker/core/middleware.py
@@ -5,7 +5,6 @@ from django.utils.deprecation import MiddlewareMixin
 
 class TimezoneMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        print("working.................................")
         tzname = request.session.get('django_timezone')
         if tzname:
             timezone.activate(pytz.timezone(tzname))
