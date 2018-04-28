217c4dad412a11ff9eb7e0f59ff5968870568099
tanvir hasan <tanvir002700@gmail.com>
2018-04-28 22:16:40 +0600

    add spec of login view get request

--- office_tracker/accounts/tests/test_views.py
+++ office_tracker/accounts/tests/test_views.py
@@ -2,13 +2,20 @@ from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import AnonymousUser
 
 from accounts.models import User
-from accounts.views import AccountDetailView
+from accounts.views import AccountDetailView, LoginView
 
 
 class SimpleTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.user = User.objects.create_user(username='jacob', email='jacob@test.com', password='top_secret')
+
+    def test_login(self):
+        request = self.factory.get('/accounts/login')
+        request.user = AnonymousUser
+
+        response = LoginView.as_view()(request)
+        self.assertEqual(response.status_code, 302)
 
     def test_details(self):
         request = self.factory.get('/accounts/detail')
