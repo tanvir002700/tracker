bb2dced65ecaae7ae5ff725026bd1507aecea014
tanvir hasan <tanvir002700@gmail.com>
2018-04-29 01:09:39 +0600

    add spec for register view

--- office_tracker/accounts/tests/test_views.py
+++ office_tracker/accounts/tests/test_views.py
@@ -15,14 +15,26 @@ class SimpleTest(TestCase):
             'password': 'top_secret'
         }
 
+    def test_registration_get(self):
+        response = self.client.get(reverse('accounts:registration'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_registration_post(self):
+        response = self.client.post(reverse('accounts:registration'), {
+            'username': 'test',
+            'email': 'test@email.com',
+            'password1': '$password_1234',
+            'password2': '$password_1234',
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(User.objects.count(), 2)
+
     def test_login_get(self):
         response = self.client.get(reverse('accounts:login'))
-
         self.assertEqual(response.status_code, 200)
 
     def test_login_post(self):
         response = self.client.post(reverse('accounts:login'), **self.credentials, follow=True)
-
         self.assertEqual(response.status_code, 200)
 
     def test_details(self):
@@ -31,3 +43,4 @@ class SimpleTest(TestCase):
 
         response = AccountDetailView.as_view()(request)
         self.assertEqual(response.status_code, 200)
+
