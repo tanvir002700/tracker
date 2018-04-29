8c36beadbd0ad7f3bd0c3d3698c0e7d55af5a2a0
tanvir hasan <tanvir002700@gmail.com>
2018-04-29 20:25:32 +0600

    create from test

---
+++ office_tracker/accounts/tests/test_forms.py
@@ -0,0 +1,7 @@
+from django.urls import reverse
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser
+
+from accounts.forms import RegistrationForm
+
+
