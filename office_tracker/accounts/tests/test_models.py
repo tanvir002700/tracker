153d73fc27cb4c4ee2328c7df5c609327ca2135b
tanvir hasan <tanvir002700@gmail.com>
2018-04-30 01:55:03 +0600

    add spec for user model

--- office_tracker/accounts/tests/test_models.py
+++ office_tracker/accounts/tests/test_models.py
@@ -9,16 +9,26 @@ class UserMixing(object):
     def create_user(self, username='test', email='test@email.com', password='top_secret'):
         return User.objects.create(username=username, email=email, password=password)
 
-    def create_attandance_without_logout(user):
-        user = create_user()
+    def create_attandance_without_logout(self, user):
         return user.attandance_set.create(enter_at=timezone.now())
 
-    def create_attandance_with_logout(user):
-        user = create_user()
+    def create_attandance_with_logout(self, user):
         return user.attandance_set.create(enter_at=timezone.now(), out_at=timezone.now())
 
 class UserModelTest(UserMixing, TestCase):
     def test_user_create(self):
         u = self.create_user()
+
         self.assertTrue(isinstance(u, User))
 
+    def test_is_active_login_true(self):
+        u = self.create_user()
+        self.create_attandance_without_logout(u)
+
+        self.assertTrue(u.is_active_login())
+
+    def test_is_active_login_false(self):
+        u = self.create_user()
+        self.create_attandance_with_logout(u)
+
+        self.assertFalse(u.is_active_login())
