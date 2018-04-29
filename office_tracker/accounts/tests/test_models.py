17e93fb7447ee8a1a867e91e82c421d40dbe4d84
tanvir hasan <tanvir002700@gmail.com>
2018-04-29 13:28:40 +0600

    add model test

--- office_tracker/accounts/tests/test_models.py
+++ office_tracker/accounts/tests/test_models.py
@@ -2,3 +2,13 @@ from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import AnonymousUser
 
 from accounts.models import User
+
+
+class AccountsTest(TestCase):
+
+    def create_user(self, username='test', email='test@email.com', password='top_secret'):
+        return User.objects.create(username=username, email=email, password=password)
+
+    def test_user_create(self):
+        u = self.create_user()
+        self.assertTrue(isinstance(u, User))
