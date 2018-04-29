e4bb0bba52d6fa76d0941421320dcb239c654a95
tanvir hasan <tanvir002700@gmail.com>
2018-04-30 01:41:55 +0600

    add mixing for users model test

--- office_tracker/accounts/tests/test_models.py
+++ office_tracker/accounts/tests/test_models.py
@@ -2,13 +2,23 @@ from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import AnonymousUser
 
 from accounts.models import User
+from daily_tracker.models import Attandance
+from django.utils import timezone
 
-
-class AccountsTest(TestCase):
-
+class UserMixing(object):
     def create_user(self, username='test', email='test@email.com', password='top_secret'):
         return User.objects.create(username=username, email=email, password=password)
 
+    def create_attandance_without_logout(user):
+        user = create_user()
+        return user.attandance_set.create(enter_at=timezone.now())
+
+    def create_attandance_with_logout(user):
+        user = create_user()
+        return user.attandance_set.create(enter_at=timezone.now(), out_at=timezone.now())
+
+class UserModelTest(UserMixing, TestCase):
     def test_user_create(self):
         u = self.create_user()
         self.assertTrue(isinstance(u, User))
+
