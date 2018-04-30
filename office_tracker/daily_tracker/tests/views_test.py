2b5ac900801cdcd3efd167433918b7a1ba5185c9
tanvir hasan <tanvir002700@gmail.com>
2018-05-01 01:04:54 +0600

    add test coverage for daily tracker success login

---
+++ office_tracker/daily_tracker/tests/views_test.py
@@ -0,0 +1,24 @@
+from django.urls import reverse
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser
+
+from accounts.models import User
+from daily_tracker.views import DailyLoginView
+
+
+class TestMixing(object):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='jacob', email='jacob@test.com', password='top_secret')
+        self.credentials = {
+            'username': 'jacob',
+            'password': 'top_secret'
+        }
+
+
+class DailyLoginViewTest(TestMixing, TestCase):
+    def test_when_user_logged_in(self):
+        self.client.login(**self.credentials)
+        response = self.client.get(reverse('daily_tracker:login'))
+
+        self.assertRedirects(response, expected_url=reverse('daily_tracker:attandance_list'), status_code=302, target_status_code=200)
