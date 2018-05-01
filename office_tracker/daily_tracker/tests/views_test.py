71fd5f943f7736ab2f77b26ed552fdc13379123f
tanvir hasan <tanvir002700@gmail.com>
2018-05-01 20:49:07 +0600

    add spec for daily logout

--- office_tracker/daily_tracker/tests/views_test.py
+++ office_tracker/daily_tracker/tests/views_test.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import AnonymousUser
+from django.utils import timezone
 
 from accounts.models import User
 from daily_tracker.views import DailyLoginView
@@ -38,3 +39,12 @@ class DailyLoginViewTest(TestMixing, TestCase):
 
         response = self.client.get(reverse('daily_tracker:login'))
         self.assertEqual(self.user.attandance_set.count(), 1)
+
+
+class DailyLogoutViewTest(TestMixing, TestCase):
+    def test_when_user_logged_in(self):
+        self.user.attandance_set.create(enter_at=timezone.now())
+        self.client.login(**self.credentials)
+
+        response = self.client.get(reverse('daily_tracker:logout'))
+        self.assertFalse(self.user.is_active_login())
