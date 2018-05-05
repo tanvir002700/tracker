e895d13af2beb783fc6dfc6a9c2da97a9d711ed2
tanvir hasan <tanvir002700@gmail.com>
2018-05-05 12:47:34 +0600

    install daily tracker app

--- office_tracker/office_tracker/settings.py
+++ office_tracker/office_tracker/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
     'daily_tracker',
+    'leave_tracker',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
