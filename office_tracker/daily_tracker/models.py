8f13fa60801fee742978479f32c1e689ceb46c1a
tanvir hasan <tanvir002700@gmail.com>
2018-05-07 22:50:06 +0600

    add total time integer field daily attandance field

--- office_tracker/daily_tracker/models.py
+++ office_tracker/daily_tracker/models.py
@@ -6,6 +6,7 @@ from accounts.models import User
 class Attandance(TimeStampedModel):
     enter_at = models.DateTimeField(null=True)
     out_at = models.DateTimeField(null=True)
+    total_time = models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
 
     def is_logged_in(self):
