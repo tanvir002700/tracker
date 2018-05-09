5084c5b7c2fc51af9a34fe55cc22df304232b15f
tanvir hasan <tanvir002700@gmail.com>
2018-05-10 01:14:25 +0600

    add project common template tag module

---
+++ office_tracker/core/templatetags/duration.py
@@ -0,0 +1,8 @@
+from django import template
+import datetime
+
+register = template.Library()
+
+@register.filter(name='duration')
+def duration(seconds):
+    return str(seconds)
--- office_tracker/office_tracker/settings.py
+++ office_tracker/office_tracker/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
+    'core',
     'daily_tracker',
     'leave_tracker',
     'django.contrib.admin',
