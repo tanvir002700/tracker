dc917a5d4c05c846d8f9b38e32ba81505df82047
tanvir hasan <tanvir002700@gmail.com>
2018-05-10 01:56:01 +0600

    fix test code

--- office_tracker/daily_tracker/tests/decorators_test.py
+++ office_tracker/daily_tracker/tests/decorators_test.py
@@ -2,10 +2,11 @@ from django.test import TestCase
 from daily_tracker.decorators import update_total_time
 from daily_tracker.models import Attandance
 from django.utils import timezone
+from datetime import timedelta
 
 class TestDecorators(TestCase):
     def test(self):
-        attandance = Attandance.objects.create(enter_at=timezone.now(), out_at=timezone.now())
+        attandance = Attandance.objects.create(enter_at=timezone.now(), out_at=timezone.now() + timedelta(hours=2))
         @update_total_time
         def mock(attandance):
             return True
