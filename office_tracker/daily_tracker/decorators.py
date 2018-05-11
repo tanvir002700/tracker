ae158f945af32e05d26144e81d0bd80912f7d5b5
tanvir hasan <tanvir002700@gmail.com>
2018-05-12 01:15:00 +0600

    update time update decorator

--- office_tracker/daily_tracker/decorators.py
+++ office_tracker/daily_tracker/decorators.py
@@ -1,11 +1,12 @@
 from datetime import timedelta
+from django.utils import timezone
 
 def update_total_time(original):
     def wrapper(*args, **kwargs):
         original(*args, **kwargs)
         self = args[0]
-        enter_date = self.enter_at.date()
-        out_date = self.out_at.date()
+        enter_date = timezone.localtime(self.enter_at).date()
+        out_date = timezone.localtime(self.out_at).date()
         self.total_time = (self.out_at - self.enter_at).total_seconds()
         if enter_date != out_date:
             self.total_time = timedelta(hours=2).total_seconds()
--- office_tracker/daily_tracker/models.py
+++ office_tracker/daily_tracker/models.py
@@ -16,12 +16,12 @@ class Attandance(TimeStampedModel):
 
     def login(self):
         if self.enter_at is None:
-            self.enter_at = timezone.localtime(timezone.now())
+            self.enter_at = timezone.now()
             self.save()
 
     @update_total_time
     def logout(self):
         if self.out_at is None:
-            self.out_at = timezone.localtime(timezone.now())
+            self.out_at = timezone.now()
             self.save()
 
