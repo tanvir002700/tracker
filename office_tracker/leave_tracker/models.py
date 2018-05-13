6656cf02ea7ac0aa637eb01b14162e0fa184ed1b
tanvir hasan <tanvir002700@gmail.com>
2018-05-13 20:00:58 +0600

    add Leave model with fileds

--- office_tracker/leave_tracker/models.py
+++ office_tracker/leave_tracker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Leave(models.Model):
+    SICK_LEAVE = 'SK'
+    CAUSAL_LEAVE = 'CA'
+    LEAVE_TYPE = (
+        (SICK_LEAVE, 'Sick Leave'),
+        (CAUSAL_LEAVE, 'Causal Leave')
+    )
+    leave_type = models.CharField(max_length=3, choices=LEAVE_TYPE, default=SICK_LEAVE)
+    leave_reason = models.TextField()
