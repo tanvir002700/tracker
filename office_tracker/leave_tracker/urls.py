4e8aec991bde4027417e437d9a3805d701340033
tanvir hasan <tanvir002700@gmail.com>
2018-05-14 20:58:45 +0600

    add success rul to leave create view

--- office_tracker/leave_tracker/urls.py
+++ office_tracker/leave_tracker/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^create/$', views.LeaveCreateView.as_view(), name='create')
+    url('^create/$', views.LeaveCreateView.as_view(), name='create'),
+    url('^leave_list/$', views.LeaveListView.as_view(), name='leave_list')
 ]
--- office_tracker/leave_tracker/views.py
+++ office_tracker/leave_tracker/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
+from django.urls import reverse_lazy
 from .models import Leave
 from .forms import LeaveForm
 
@@ -8,6 +9,7 @@ class LeaveCreateView(CreateView):
     model = Leave
     template_name = 'leave_tracker/leave_create_form.html'
     form_class = LeaveForm
+    success_url = reverse_lazy('leave_tracker:leave_list')
 
 
 class LeaveListView(ListView):
