51a807314d2f5dc472bc4c49438351405fc6db90
tanvir hasan <tanvir002700@gmail.com>
2018-05-14 20:28:43 +0600

    add leave tracker list view

--- office_tracker/leave_tracker/views.py
+++ office_tracker/leave_tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from .models import Leave
 from .forms import LeaveForm
 
@@ -7,3 +8,8 @@ class LeaveCreateView(CreateView):
     model = Leave
     template_name = 'leave_tracker/leave_create_form.html'
     form_class = LeaveForm
+
+
+class LeaveListView(ListView):
+    model = Leave
+    template_name = 'leave_tracker/leave_list.html'
