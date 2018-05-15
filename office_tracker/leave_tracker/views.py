999d628258f971102d21a3242e05efa0cbb26d9c
tanvir hasan <tanvir002700@gmail.com>
2018-05-15 22:40:18 +0600

    add authentication leave tracker

--- office_tracker/leave_tracker/views.py
+++ office_tracker/leave_tracker/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Leave
 from .forms import LeaveForm
 
-class LeaveCreateView(CreateView):
+class LeaveCreateView(LoginRequiredMixin, CreateView):
     model = Leave
     template_name = 'leave_tracker/leave_create_form.html'
     form_class = LeaveForm
     success_url = reverse_lazy('leave_tracker:leave_list')
 
 
-class LeaveListView(ListView):
+class LeaveListView(LoginRequiredMixin, ListView):
     model = Leave
     template_name = 'leave_tracker/leave_list.html'
