42fb367067c374dadd65d67442e318bea4de8e40
tanvir hasan <tanvir002700@gmail.com>
2018-05-18 00:17:30 +0600

    add date picker

--- office_tracker/leave_tracker/forms.py
+++ office_tracker/leave_tracker/forms.py
@@ -5,7 +5,7 @@ from django import forms
 
 class LeaveForm(ModelForm):
     leave_reason = forms.CharField(required=True, widget=forms.Textarea)
-    date_from = forms.DateField(widget=forms.SelectDateWidget)
+    date_from = forms.DateField(widget=forms.TextInput(attrs={'class': 'datepicker'}))
     date_to = forms.DateField(widget=forms.SelectDateWidget)
 
     class Meta:
