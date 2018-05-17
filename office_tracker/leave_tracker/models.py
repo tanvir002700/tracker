ff76c0d57e40a3bff9fbf5b968925e0fa28638b8
tanvir hasan <tanvir002700@gmail.com>
2018-05-17 20:20:26 +0600

    generate migration for leave tracker

--- office_tracker/leave_tracker/models.py
+++ office_tracker/leave_tracker/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Leave(models.Model):
     SICK_LEAVE = 'SK'
@@ -10,8 +11,8 @@ class Leave(models.Model):
 
     leave_type = models.CharField(max_length=3, choices=LEAVE_TYPE, default=SICK_LEAVE)
     leave_reason = models.TextField()
-    date_from = models.DateField()
-    date_to = models.DateField()
+    date_from = models.DateField(default=timezone.now())
+    date_to = models.DateField(default=timezone.now())
 
     def __str__(self):
         return self.leave_reason
