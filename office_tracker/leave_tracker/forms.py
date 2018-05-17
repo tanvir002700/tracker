6b0738ee94529f77a1acd33c62443b4f5ef3337e
tanvir hasan <tanvir002700@gmail.com>
2018-05-17 21:10:42 +0600

    add datefield widget

--- office_tracker/leave_tracker/forms.py
+++ office_tracker/leave_tracker/forms.py
@@ -5,7 +5,9 @@ from django import forms
 
 class LeaveForm(ModelForm):
     leave_reason = forms.CharField(required=True, widget=forms.Textarea)
+    date_from = forms.DateField(widget=forms.SelectDateWidget)
+    date_to = forms.DateField(widget=forms.SelectDateWidget)
 
     class Meta:
         model = Leave
-        fields = ['leave_type', 'leave_reason']
+        fields = ['leave_type', 'leave_reason', 'date_from', 'date_to']
