242fd7039309be88969e727cc4756ae165953162
tanvir hasan <tanvir002700@gmail.com>
2018-05-19 02:18:43 +0600

    refactor urls

--- office_tracker/leave_tracker/urls.py
+++ office_tracker/leave_tracker/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.LeaveListView.as_view(), name='leave_list'),
+    path('<int:pk>', views.LeaveDetailView.as_view(), name='detail'),
     path('create/', views.LeaveCreateView.as_view(), name='create'),
     path('<int:pk>/edit/', views.LeaveUpdateView.as_view(), name='update'),
     path('<int:pk>/delete/', views.LeaveDeleteView.as_view(), name='delete'),
-    path('', views.LeaveListView.as_view(), name='leave_list'),
-    path('<int:pk>', views.LeaveDetailView.as_view(), name='detail'),
 ]
