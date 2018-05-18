52407304e41b39e745cfa9861a057fb942c63ebf
tanvir hasan <tanvir002700@gmail.com>
2018-05-18 20:23:10 +0600

    add delete view

--- office_tracker/leave_tracker/urls.py
+++ office_tracker/leave_tracker/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('create', views.LeaveCreateView.as_view(), name='create'),
     path('<int:pk>', views.LeaveUpdateView.as_view(), name='update'),
-    path('leave_list', views.LeaveListView.as_view(), name='leave_list')
+    path('leave_list', views.LeaveListView.as_view(), name='leave_list'),
+    path('delete/<int:pk>', views.LeaveDeleteView.as_view(), name='delete')
 ]
--- office_tracker/leave_tracker/views.py
+++ office_tracker/leave_tracker/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Leave
 from .forms import LeaveForm
+
+
+class LeaveListView(LoginRequiredMixin, ListView):
+    model = Leave
+    template_name = 'leave_tracker/leave_list.html'
+
 
 class LeaveCreateView(LoginRequiredMixin, CreateView):
     model = Leave
@@ -21,6 +28,8 @@ class LeaveUpdateView(LoginRequiredMixin, UpdateView):
     success_url = reverse_lazy('leave_tracker:leave_list')
 
 
-class LeaveListView(LoginRequiredMixin, ListView):
+class LeaveDeleteView(LoginRequiredMixin, DeleteView):
     model = Leave
-    template_name = 'leave_tracker/leave_list.html'
+    form_class = LeaveForm
+    success_url = reverse_lazy('leave_tracker:leave_list')
+
