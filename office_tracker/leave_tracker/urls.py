5e7d5a3ea72f0f634f3a81b611b2cb15edc50053
tanvir hasan <tanvir002700@gmail.com>
2018-05-18 20:06:11 +0600

    update leave tracker urls with path method

--- office_tracker/leave_tracker/urls.py
+++ office_tracker/leave_tracker/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url('^create/$', views.LeaveCreateView.as_view(), name='create'),
-    url('^leave_list/$', views.LeaveListView.as_view(), name='leave_list')
+    path('create', views.LeaveCreateView.as_view(), name='create'),
+    path('leave_list', views.LeaveListView.as_view(), name='leave_list'),
+    path('update/<int:pk>', views.LeaveUpdateView.as_view(), name='update')
 ]
--- office_tracker/leave_tracker/views.py
+++ office_tracker/leave_tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
@@ -7,6 +8,13 @@ from .models import Leave
 from .forms import LeaveForm
 
 class LeaveCreateView(LoginRequiredMixin, CreateView):
+    model = Leave
+    template_name = 'leave_tracker/leave_create_form.html'
+    form_class = LeaveForm
+    success_url = reverse_lazy('leave_tracker:leave_list')
+
+
+class LeaveUpdateView(LoginRequiredMixin, UpdateView):
     model = Leave
     template_name = 'leave_tracker/leave_create_form.html'
     form_class = LeaveForm
