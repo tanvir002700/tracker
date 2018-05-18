bfa1a416b771c16d800cac682df262487ccd37eb
tanvir hasan <tanvir002700@gmail.com>
2018-05-18 23:36:31 +0600

    refactor leave tracker urls

--- office_tracker/leave_tracker/urls.py
+++ office_tracker/leave_tracker/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('create/', views.LeaveCreateView.as_view(), name='create'),
-    path('edit/<int:pk>/', views.LeaveUpdateView.as_view(), name='update'),
-    path('delete/<int:pk>/', views.LeaveDeleteView.as_view(), name='delete'),
-    path('leave_list/', views.LeaveListView.as_view(), name='leave_list'),
+    path('<int:pk>/edit/', views.LeaveUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.LeaveDeleteView.as_view(), name='delete'),
+    path('', views.LeaveListView.as_view(), name='leave_list'),
     path('<int:pk>', views.LeaveDetailView.as_view(), name='detail'),
 ]
