efae8f38d80bfee922451a89cd346c765d9ca682
tanvir hasan <tanvir002700@gmail.com>
2018-05-18 22:15:30 +0600

    add detail view

--- office_tracker/leave_tracker/urls.py
+++ office_tracker/leave_tracker/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('edit/<int:pk>/', views.LeaveUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', views.LeaveDeleteView.as_view(), name='delete'),
     path('leave_list/', views.LeaveListView.as_view(), name='leave_list'),
+    path('<int:pk>', views.LeaveDetailView.as_view(), name='detail'),
 ]
--- office_tracker/leave_tracker/views.py
+++ office_tracker/leave_tracker/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
@@ -12,6 +13,12 @@ from .forms import LeaveForm
 class LeaveListView(LoginRequiredMixin, ListView):
     model = Leave
     template_name = 'leave_tracker/leave_list.html'
+
+
+class LeaveDetailView(LoginRequiredMixin, DetailView):
+    model = Leave
+    template_name = 'leave_tracker/leave_detail.html'
+    context_object_name = 'leave'
 
 
 class LeaveCreateView(LoginRequiredMixin, CreateView):
