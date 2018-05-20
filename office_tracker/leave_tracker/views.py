41e95057abe68dd0916b6d929c7d6643107dd098
tanvir hasan <tanvir002700@gmail.com>
2018-05-20 20:21:28 +0600

    add validation for leave object update view

--- office_tracker/leave_tracker/views.py
+++ office_tracker/leave_tracker/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from django.urls import reverse_lazy
 from .models import Leave
 from .forms import LeaveForm
@@ -33,6 +34,16 @@ class LeaveUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'leave_tracker/leave_form.html'
     form_class = LeaveForm
     success_url = reverse_lazy('leave_tracker:leave_list')
+    success_message = 'successfully saved!!!!'
+
+    def dispatch(self, request, *args, **kwargs):
+        object = self.get_object()
+        if (object.status != Leave.PENDING):
+            messages.add_message(self.request, messages.WARNING, 'cant update', 'danger')
+            return redirect(reverse_lazy('leave_tracker:leave_list'))
+        messages.add_message(self.request, messages.INFO, 'successfully update')
+        return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
+
 
 
 class LeaveDeleteView(LoginRequiredMixin, DeleteView):
