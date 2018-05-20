5b495d5073d78a08698c10f264341afcc3713ad5
tanvir hasan <tanvir002700@gmail.com>
2018-05-21 01:50:43 +0600

    add spce to leave type

--- office_tracker/leave_tracker/tests/models_test.py
+++ office_tracker/leave_tracker/tests/models_test.py
@@ -7,7 +7,13 @@ class TestMixing(object):
     def setUp(self):
         self.sick_leave = Leave.objects.create(leave_type=Leave.SICK_LEAVE, leave_reason='test',
                                                      date_from=datetime.now(), date_to=datetime.now())
+        self.casual_leave = Leave.objects.create(leave_type=Leave.CAUSAL_LEAVE, leave_reason='test',
+                                                     date_from=datetime.now(), date_to=datetime.now())
 
 class LeaveTest(TestMixing, TestCase):
     def test_default_value(self):
         self.assertEqual(self.sick_leave.status, Leave.PENDING)
+
+    def test_leave_type(self):
+        self.assertEqual(self.sick_leave.leave_type, Leave.SICK_LEAVE)
+        self.assertEqual(self.casual_leave.leave_type, Leave.CAUSAL_LEAVE)
