00ea3b8f2c397533fc5f5728e82690b2c5d8a47c
tanvir hasan <tanvir002700@gmail.com>
2018-05-21 23:43:06 +0600

    create test mixing for leave tracker view

---
+++ office_tracker/leave_tracker/tests/views_test.py
@@ -0,0 +1,19 @@
+from django.urls import reverse
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser
+from datetime import datetime
+
+from leave_tracker.models import Leave
+
+
+class TestMixing(object):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='jacob',
+                                             email='jacob@test.com',
+                                             password='top_secret')
+        self.credentials = {
+            'username': 'jacob',
+            'password': 'top_secret'
+        }
+
