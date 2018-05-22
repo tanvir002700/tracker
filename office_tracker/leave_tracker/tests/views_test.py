68394549941fb8ad33fcb899a019200892d5312c
tanvir hasan <tanvir002700@gmail.com>
2018-05-22 20:56:24 +0600

    add authorization test for leave tracker list view

--- office_tracker/leave_tracker/tests/views_test.py
+++ office_tracker/leave_tracker/tests/views_test.py
@@ -3,7 +3,9 @@ from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import AnonymousUser
 from datetime import datetime
 
+from accounts.models import User
 from leave_tracker.models import Leave
+from leave_tracker.views import LeaveListView
 
 
 class TestMixing(object):
@@ -18,8 +20,17 @@ class TestMixing(object):
         }
 
 
-class TestLeaveListView(TestCase):
-    pass
+class TestLeaveListView(TestMixing, TestCase):
+    def test_unauthorized_access(self):
+        response = self.client.get(reverse('leave_tracker:leave_list'))
+        self.assertRedirects(response, expected_url='/accounts/login/?next=/leave_tracker/', status_code=302, target_status_code=200)
+
+    def test_authorize_access(self):
+        self.client.login(**self.credentials)
+        self.client.get(reverse('daily_tracker:login'))
+
+        response = self.client.get(reverse('leave_tracker:leave_list'))
+        self.assertTrue(response.status_code, 200)
 
 
 class TestLeaveDetailView(TestCase):
