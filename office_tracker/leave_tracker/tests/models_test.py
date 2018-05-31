569597da53abb77eab97f058b3f8f2d013d05899
tanvir hasan <tanvir002700@gmail.com>
2018-05-31 19:49:38 +0600

    fix spec for leave tracker Leave model

--- office_tracker/leave_tracker/tests/models_test.py
+++ office_tracker/leave_tracker/tests/models_test.py
@@ -1,14 +1,19 @@
 from datetime import datetime
 from django.test import TestCase
 
-from leave_tracker.models import Leave
+from accounts.models import User
+from leave_tracker.models import Leave, Season
 
 class TestMixing(object):
     def setUp(self):
+        self.season = Season.objects.create()
+        self.user = User.objects.create_user(username='jacob', email='jacob@test.com', password='top_secret')
         self.sick_leave = Leave.objects.create(leave_type=Leave.SICK_LEAVE, leave_reason='test',
-                                               date_from=datetime.now(), date_to=datetime.now())
+                                               date_from=datetime.now(), date_to=datetime.now(),
+                                               user_season=self.user.userseason_set.last())
         self.casual_leave = Leave.objects.create(leave_type=Leave.CAUSAL_LEAVE, leave_reason='test',
-                                                 date_from=datetime.now(), date_to=datetime.now())
+                                                 date_from=datetime.now(), date_to=datetime.now(),
+                                                 user_season=self.user.userseason_set.last())
 
 class LeaveTest(TestMixing, TestCase):
     def test_default_value(self):
