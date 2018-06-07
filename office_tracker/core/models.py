2f1787af929a786d62ecfa142368d1e7a9bd5008
tanvir hasan <tanvir002700@gmail.com>
2018-06-07 21:12:04 +0600

    update core model

--- office_tracker/core/models.py
+++ office_tracker/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class TimeStampedModel(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
     modified = models.DateTimeField(auto_now=True)
 
     class Meta(object):
