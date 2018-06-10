b4c5365bc4865ea1c7ce8cae5b62d593fb164a9f
tanvir hasan <tanvir002700@gmail.com>
2018-06-11 01:42:13 +0600

    implement test signal separetly

--- office_tracker/leave_tracker/apps.py
+++ office_tracker/leave_tracker/apps.py
@@ -1,10 +1,11 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_save
-
+from .signals import test_signals
 
 class LeaveTrackerConfig(AppConfig):
     name = 'leave_tracker'
 
     def ready(self):
+        from .models import Season
         print("execute ready")
-        import leave_tracker.signals
+        post_save.connect(test_signals, sender=Season)
