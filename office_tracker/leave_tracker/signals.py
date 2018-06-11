43992c7f455a0e8be42434f4e8e3d9f31fa0f4f8
tanvir hasan <tanvir002700@gmail.com>
2018-06-11 22:15:27 +0600

    refactor signals

--- office_tracker/leave_tracker/signals.py
+++ office_tracker/leave_tracker/signals.py
@@ -1,12 +1,12 @@
 from django.apps import apps
 
-def assign_season_to_all_user(sender, **kwargs):
+def assign_season_to_all_user(sender, **_kwargs):
     print("run signals for season...........")
-    instance = kwargs.get('instance')
+    print(sender)
     User = apps.get_model('accounts', 'User')
     UserSeason = apps.get_model('leave_tracker', 'UserSeason')
     users = User.objects.all()
     user_seasons = []
     for user in users:
-        user_seasons.append(UserSeason(user=user, season=instance))
+        user_seasons.append(UserSeason(user=user, season=sender))
     UserSeason.objects.bulk_create(user_seasons)
