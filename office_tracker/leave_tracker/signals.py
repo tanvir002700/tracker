de796975a8e45ceb9626b5a566a351a9fd222019
tanvir hasan <tanvir002700@gmail.com>
2018-06-16 21:30:48 +0600

    fix signals

--- office_tracker/leave_tracker/signals.py
+++ office_tracker/leave_tracker/signals.py
@@ -1,10 +1,11 @@
 from django.apps import apps
 
-def assign_season_to_all_user(sender, **_kwargs):
+def assign_season_to_all_user(sender, **kwargs):
+    instance = kwargs.get('instance')
     User = apps.get_model('accounts', 'User')
     UserSeason = apps.get_model('leave_tracker', 'UserSeason')
     users = User.objects.all()
     user_seasons = []
     for user in users:
-        user_seasons.append(UserSeason(user=user, season=sender))
+        user_seasons.append(UserSeason(user=user, season=instance))
     UserSeason.objects.bulk_create(user_seasons)
