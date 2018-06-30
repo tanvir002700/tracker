3056303948398a2ddf422ad5f758446b04ed9c94
tanvir hasan <tanvir002700@gmail.com>
2018-06-30 22:08:16 +0600

    add docker settings

---
+++ office_tracker/office_tracker/settings/docker.py
@@ -0,0 +1,27 @@
+import os
+from .base import *
+
+SECRET_KEY = '!ok^nac(io_tz+%kc0y&rj)a@1y04@&=g7+#u(_j#$6x^=c**+'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'traker',
+        'USER': 'django',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '',
+    }
+}
