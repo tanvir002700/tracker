a24144a57efa557015b3a52ac12c06d4c1eb7b7e
tanvir hasan <tanvir002700@gmail.com>
2018-07-21 17:49:02 +0600

    update attandance serializer

--- office_tracker/api/daily_tracker/serializers.py
+++ office_tracker/api/daily_tracker/serializers.py
@@ -2,14 +2,7 @@ from rest_framework import serializers
 from daily_tracker.models import Attandance
 
 
-class AttandanceSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    enter_at = serializers.DateTimeField(allow_null=True)
-    out_at = serializers.DateTimeField(allow_null=True)
-    total_time = serializers.IntegerField(default=0)
-
-    def create(self, validated_data):
-        return Attandance.object.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        return instance
+class AttandanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attandance
+        fields = ('id', 'enter_at', 'out_at', 'total_time')
