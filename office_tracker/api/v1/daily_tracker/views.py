7da64990517bef0790e698a5957fa593c4625779
tanvir hasan <tanvir002700@gmail.com>
2018-07-22 02:26:57 +0600

    add permission to attandance list viewset

--- office_tracker/api/v1/daily_tracker/views.py
+++ office_tracker/api/v1/daily_tracker/views.py
@@ -2,8 +2,10 @@ from rest_framework import generics
 from daily_tracker.models import Attandance
 from .serializers import AttandanceSerializer
 from rest_framework import viewsets
+from rest_framework import permissions
 
 
 class AttandanceList(viewsets.ReadOnlyModelViewSet):
     queryset = Attandance.objects.all()
     serializer_class = AttandanceSerializer
+    permission_classes = (permissions.IsAuthenticated,)
