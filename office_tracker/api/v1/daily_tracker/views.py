a5fbadc279f544f7f4735c2d7661c778d7d7b8de
tanvir hasan <tanvir002700@gmail.com>
2018-07-22 01:00:45 +0600

    Update attandance view with generic view:

--- office_tracker/api/v1/daily_tracker/views.py
+++ office_tracker/api/v1/daily_tracker/views.py
@@ -1,11 +1,8 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 from daily_tracker.models import Attandance
 from .serializers import AttandanceSerializer
 
 
-class AttandanceList(APIView):
-    def get(self, request):
-        attandances = Attandance.objects.all()
-        serializer = AttandanceSerializer(attandances, many=True)
-        return Response(serializer.data)
+class AttandanceList(generics.ListAPIView):
+    queryset = Attandance.objects.all()
+    serializer_class = AttandanceSerializer
