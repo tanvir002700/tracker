aa4c3f4f67990a96db71adec52042ed88394f4ba
tanvir hasan <tanvir002700@gmail.com>
2018-07-21 22:35:05 +0600

    add independent urls file for daily tracker

--- office_tracker/api/daily_tracker/urls.py
+++ office_tracker/api/daily_tracker/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .daily_tracker.views import AttandanceList
+from .views import AttandanceList
 
 
 urlpatterns = [
