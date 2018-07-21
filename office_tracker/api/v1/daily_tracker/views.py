445826caa68acfccab5c041605495e3627caf339
tanvir hasan <tanvir002700@gmail.com>
2018-07-22 02:28:06 +0600

    refactor code

--- office_tracker/api/v1/daily_tracker/views.py
+++ office_tracker/api/v1/daily_tracker/views.py
@@ -1,11 +1,10 @@
-from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from daily_tracker.models import Attandance
 from .serializers import AttandanceSerializer
-from rest_framework import viewsets
-from rest_framework import permissions
 
 
 class AttandanceList(viewsets.ReadOnlyModelViewSet):
     queryset = Attandance.objects.all()
     serializer_class = AttandanceSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
