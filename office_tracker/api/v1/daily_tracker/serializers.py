b64b7cb053b7622c67a8142f2849d56423f30f2a
tanvir002700 <tanvir002700@gmail.com>
2018-07-23 12:25:00 +0600

    add hyperlinkedmodel filed to attandance

--- office_tracker/api/v1/daily_tracker/serializers.py
+++ office_tracker/api/v1/daily_tracker/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from daily_tracker.models import Attandance
 
 
-class AttandanceSerializer(serializers.ModelSerializer):
+class AttandanceSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api:attandance-detail')
     class Meta:
         model = Attandance
-        fields = ('id', 'enter_at', 'out_at', 'total_time')
+        fields = ('id', 'enter_at', 'out_at', 'total_time', 'url')
